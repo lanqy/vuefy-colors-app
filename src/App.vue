47dcffc3bc45b7f1b7fd3d814886e7e2b990fa89
Lan QingYong <lanqy.me@gmail.com>
2017-04-28 01:27:37 +0800

    add file

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <ul>
-      <router-link to="/">Colors</router-link>
-      <router-link to="/users">Users</router-link>
-    </ul>
     <router-view></router-view>
   </div>
 </template>
@@ -16,15 +12,14 @@ export default {
 
 <style>
 *{margin:0;padding:0;}
-html,body{width: 100%;height: 100%;}
+html,body{height: 100%;width:100%}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   height: 100%;
   width: 100%;
-  transition: all 2s ease;
+  color: #2c3e50;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Color from '@/components/color'
-import Users from '@/components/users'
 
 Vue.use(Router)
 
@@ -9,14 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'color',
+      name: 'Color',
       component: Color
-    },
-    {
-      path: '/users',
-      name: 'users',
-      component: Users
     }
-  ],
-  mode: 'history'
+  ]
 })
